d07499bdcf31ca4ff4043eb21374f5a656a2861b
Harshad Kulkarni <57728798+HarshadKulkarni@users.noreply.github.com>
2024-04-14 16:51:58 -0500

    adding 2 codes today

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This repo will have my python codes
+
+Sunday April 14 2024
+Comitting "vowels.py" & "do_you_like_tests.py" codes
---
+++ do_you_like_test.py
@@ -0,0 +1,5 @@
+answer = input('do you like tests?')
+if answer == "yes":
+    print('ok,cool!')
+else:
+    print('thats fine,')
