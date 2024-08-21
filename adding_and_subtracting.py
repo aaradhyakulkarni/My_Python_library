7de9762686b330054991b906b0536e5f69079335
Harshad Kulkarni <57728798+HarshadKulkarni@users.noreply.github.com>
2024-08-21 08:58:53 -0400

    test

---
+++ adding_and_subtracting.py
@@ -0,0 +1,5 @@
+question = input('Hi! Im going to help you add or subtract. What do you want to do?' )
+if question is 'add' or 'adding':
+   num1 = input((int)'first number')
+   num2 = input((int)'second number')
+   num1 + num2 
